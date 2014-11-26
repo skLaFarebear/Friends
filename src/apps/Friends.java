08d200f79628f24c1aecc6b9cab6dccf8583265c
skLaFarebear <awadallah.farris@gmail.com>
2014-11-25 19:55:22 -0500

    Friends project
    Assignment 5

---
+++ src/apps/Friends.java
@@ -0,0 +1,24 @@
+//@author Farris Awadallah and Leon Chen
+package apps;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Friends{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Enter 1 to find the students at the school");
+		System.out.println("Enter 2 to find the shortest chain from a person");
+		System.out.println("Enter 3 to find the cliques at each school");
+		System.out.println("Enter 4 to find the connectors");
+		System.out.println("Enter 5 to quit");
+		System.out.print("Enter here: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a);
+		
+	}
+	
+}
