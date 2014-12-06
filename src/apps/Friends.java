505edd73f1726927e588528d88696f352cf04f9a
Leon Chen <lwc28@kill.cs.rutgers.edu>
2014-12-06 12:29:28 -0500

    Done with drivers, working on graph loading...

--- src/apps/Friends.java
+++ src/apps/Friends.java
@@ -9,44 +9,223 @@ public class Friends{
 	
 	public static void main(String[] args){
 		
-		Scanner sc = new Scanner(System.in);
+		// Creates a new scanner for user input
+		Scanner userInput = new Scanner(System.in);
 		
-		int friendSize; 
+		// # of students in a file
+		int totFriendSize; 
+		FriendNode deGraph[];
 		
 		System.out.print("Enter file name: ");
 		
-		try{
-			FileReader newFile = new FileReader(sc.next());
-			BufferedReader line = new BufferedReader(newFile);
-			try{
-				friendSize = Integer.parseInt(line.readLine());
-				String linedude = line.readLine();
-				while(linedude!=null) {
+		try {
+			FileReader newFile = new FileReader(userInput.next()); // Sets file location
+			BufferedReader wholeFile = new BufferedReader(newFile); // Takes buffer of file 
+			
+			try {
+				totFriendSize = Integer.parseInt(wholeFile.readLine());
+				//System.out.print(friendSize);
+				
+				// Checks if graph is empty
+				if(totFriendSize <= 0) {
+					System.out.println("No Graph Exists! No Students Found!");
+					return;
+				}
+				
+				//Starts making graph
+				deGraph = new FriendNode[totFriendSize];
+				
+				
+				int stuCount = 0;
+				
+				do {
+					
+					// New Line of file
+					String nextLine = wholeFile.readLine();
+					
+					// Checks if line is null
+					if (nextLine == null) {
+						System.out.println(": Don't lie about the number of students in file!: C1");
+						return;
+					}
+					
+					// Parsed values of the line
+					StringTokenizer lineParsed = new StringTokenizer(nextLine, "|"); 
+					
+					
+					// While we are guaranteed that the list are still students, checks for less than 2 inputs and more than 3.
+					int tokenAmount = lineParsed.countTokens();
+					
+					if (tokenAmount < 2 || tokenAmount > 3) {
+						System.out.print(": Either too many or too little input values!: C1.1");
+						return;
+					}
+					
+					// Distributed values for parsed line
+					String person = lineParsed.nextToken().toLowerCase(); // person  
+					String schoolBool = lineParsed.nextToken().toLowerCase(); // yes or no
+					
+					// Setting up new nodes
+					FriendNode newNode = new FriendNode(person, stuCount); 
+					//System.out.print(person);
+					//System.out.print(schoolBool);
+					
+					if(schoolBool.equals("y")) {
+						
+						// Checks if a school portion is added. 
+						if(!lineParsed.hasMoreTokens()) {
+							System.out.print(": This student should have a school!: C1.2");
+							return;
+						}
+						
+						String school = lineParsed.nextToken().toLowerCase();
+						
+						newNode.school = school; 
+						//System.out.print(school);
+					} else {
+						// While we are still guaranteed a good list of names. Checks if second field is anything other than n. 
+						if(!schoolBool.equals("n")) {
+							System.out.println(": Field requires y or n or it's missing first field or wrong people size!: C1.3");
+							return;
+						}
+						
+						// If no school, but there are more words. Harsh check
+						if(lineParsed.hasMoreTokens()) {
+							System.out.print(": Shouldn't have anymore things in here: C1.4");
+							return;
+						}
+					}
+					
+					deGraph[stuCount] = newNode;
+					
+					System.out.println(deGraph[stuCount]);
+					
+					++stuCount; // Increments student count
+					
+				} while (stuCount < totFriendSize);
+				
+				// For relationships between people
+				String relationLine = wholeFile.readLine();
+				int oneIdx = 0;
+				int twoIdx = 0;
+				
+				while(relationLine != null) {
+					
+					StringTokenizer lineParsed = new StringTokenizer(relationLine, "|"); 
+					
+					// Parsed values from line 
+					String relationOne = lineParsed.nextToken().toLowerCase();
+					String relationTwo = lineParsed.nextToken().toLowerCase();
+					
+					// Checking if # of people overlaps with relationships 
+					if(relationTwo.equals("y") || relationTwo.equals("n")) {
+						System.out.println(": Adjust the number of students at start of file!: C2: ");
+						return;
+					} 
+					
+					for (int oneCnt = 0; oneCnt <= totFriendSize; oneCnt++) {
+						
+						// Checking if person exists 
+						if(oneCnt == totFriendSize) {System.out.println(relationOne + " does not exist! C2.1"); return;}
+						
+						if(relationOne.equals(deGraph[oneCnt].name)) {
+							oneIdx = oneCnt;
+							break;
+						} 
+					}
+					
+					for (int twoCnt = 0; twoCnt <= totFriendSize; twoCnt++) {
+						
+						// Checking if person two exists 
+						if(twoCnt == totFriendSize) {System.out.println(relationOne + " does not exist! C2.2"); return;}
+						
+						if(relationTwo.equals(deGraph[twoCnt].name)) {
+							
+							twoIdx = twoCnt;
+							
+							break;
+						} 
+					}
+					
+					/*// Sets head of graph
+					
+					if(deGraph[oneIdx].adjList == null || deGraph[twoIdx].adjList == null) {
+						deGraph[oneIdx].adjList = new Neighbor(twoIdx, null);
+						deGraph[twoIdx].adjList = new Neighbor(oneIdx, null);
+						System.out.print("set");
+						continue;
+					}
+
+					
+					for (Neighbor pntOne = deGraph[oneIdx].adjList; pntOne == null; pntOne = pntOne.next) {
+						
+						if(pntOne.next == null) {
+							pntOne.next.next = new Neighbor(twoIdx, null);
+							System.out.print("add to end?");
+						}
+					}
+
+					
+					System.out.println(oneIdx + " " + twoIdx);
+					System.out.println(relationOne + " " + relationTwo);*/
+					
+				
+					relationLine = wholeFile.readLine();
+					
+				} 
+				
+				//System.out.println(oneIdx + " " + twoIdx);
+				
+				//System.out.print(Neighbor.in);
+				
+				/*// Goes through every line of file
+				while(linedude != null) {
+					
 					StringTokenizer Friender = new StringTokenizer(linedude, "|");
-					FriendNode newNode = new FriendNode(Friender.nextToken());  //name of each person in line
+					
+					FriendNode newNode = new FriendNode(Friender.nextToken());  // name of each person in line
+					
 					String nextToken = Friender.nextToken();
-					if(nextToken.equals("y")) {   //if they're in school or not
+					
+					if(nextToken.equals("y")) {   // if they're in school or not
+						
 						newNode.school = Friender.nextToken();
 						System.out.println(newNode.toString());
-					}else if(nextToken.equals("n")){
-						System.out.println(newNode.toString());
-					}else{
 						
+					} else if(nextToken.equals("n")){
+						
+						System.out.println(newNode);
+						
+					} else {
+						//System.out.print("hi");
 						break;
 					}
-					linedude = line.readLine();
+					
+					linedude = wholeFile.readLine();
+					++stuCount;
 				}
+				
 				while(linedude!=null){
 					
-				}
-			}catch(IOException e) {
-				System.out.println(e + ": Catch Wrong Name");
-				System.out.println("shit");
+				}*/
+				
+			} catch(NumberFormatException e) {
+				
+				System.out.println(e + ": First line of document should be number.");
+				System.out.println("Fix the damn file.");
 				return;
+				
+			} catch(IOException e) {
+				
+				System.out.println(e + ": I wonder...");
+				System.out.println("Something wrong with input output.");
+				return;
+				
 			}
-		}catch(FileNotFoundException e) {
-			System.out.println(e + ": Catch Wrong Name");
-			System.out.println("shit");
+			
+		} catch(FileNotFoundException e) {
+			System.out.println(e + ": Wrong document name.");
+			System.out.println("Type in the right document name!");
 			return;
 			
 		}
@@ -118,26 +297,45 @@ public class Friends{
 		
 		// check check sss 
 		// check farris's check
-		// Everything should be working at this point. 
+		// Everything should be working at this point.
+		
+		
 	}
 	
 }
 
 class FriendNode{
 	
-	FriendNode head;
 	String name;
 	String school;
-	FriendNode edge;
+	int idxNum;
 	
-	public FriendNode(String name){
+	Neighbor adjList;
+	
+	public FriendNode(String name, int idxNum){
 		this.name = name;
+		this.idxNum = idxNum;
 	}
 	
 	public String toString(){
-		String FrienderNode = "(" + name + ", " + school + ")";
+		String FrienderNode = "(" + name + ", " + school + "," + idxNum + ")";
 		return FrienderNode;
 		
 	}
 	
 }
+
+class Neighbor {
+	
+	int vertexNum;
+	Neighbor next; 
+	
+	public Neighbor(int num, Neighbor nbr) {
+		this.vertexNum = num;
+		this.next = nbr; 
+	}
+
+}
+
+
+
